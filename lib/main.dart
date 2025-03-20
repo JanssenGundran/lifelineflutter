414764e749c1d36dbe07d433f447b8f4f5db8586
jamg <gundranjanjan@gmail.com>
2025-03-20 16:22:30 +0800

    updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
-import 'contact.dart';
-import 'jobboard.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,28 +11,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
-    );
-  }
-}
-
-class MainScreen extends StatefulWidget {
-  @override
-  _MainScreenState createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 1;
-
-  final List<Widget> _screens = [
-    HomeScreen(),
-    JobScreen(),
-    ContactScreen(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _screens[_selectedIndex]
     );
   }
 }
